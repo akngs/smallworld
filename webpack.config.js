cb15ab26dcb8c5155646d293cf454bd06c0f9467
akngs <jania902@gmail.com>
2018-08-31 16:32:31 +0900

    Enable github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js',
     library: 'sw',
     libraryTarget: 'window'
