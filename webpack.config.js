10095943dbee4022186ee830a0a5369dec811b30
akngs <jania902@gmail.com>
2018-09-01 22:17:43 +0900

    Use babel/preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const path = require('path')
 
 module.exports = {
   entry: './js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js',
