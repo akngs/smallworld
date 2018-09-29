ed69502d3196a4d70f48b873d617b7ae17551e38
akngs <jania902@gmail.com>
2018-09-29 14:52:00 +0900

    Update data

--- js/index.ts
+++ js/index.ts
@@ -3,7 +3,7 @@ import * as d3 from 'd3'
 import Awesomplete from 'awesomplete'
 import {Graph, GraphNode, GraphRenderer, Loader, PersonNode} from './graph'
 
-const DATA_HASH = '49b64fe'
+const DATA_HASH = '031de3e'
 
 let renderer: GraphRenderer
 
